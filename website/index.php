dac13669ac01b1d02cf4851e6b80145f0b94c4f5
Hermeto Oliveira <hermeto.co3@gmail.com>
2022-10-18 23:03:08 -0300

    Trabahando com BootStrap

--- website/index.php
+++ website/index.php
@@ -5,31 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SADLPV - Trabalho de Conclusão de Curso</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"/>
 </head>
 <body>
     <?php
         $result = file_get_contents("http://node-container:9001/veiculos");
         $veiculos = json_decode($result)
     ?>
-    <table>
-        <thead>
-            <tr>
-                <th>Proprietário</th>
-                <th>Placa</th>
-                <th>Chassi</th>
-                <th>situação</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($veiculos as $veiculo): ?>
+
+    <div class="container">
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><?php echo $veiculo->proprietario; ?></td>
-                    <td><?php echo $veiculo->placa; ?></td>
-                    <td><?php echo $veiculo->chassi; ?></td>
-                    <td><?php echo $veiculo->situacao; ?></td>                    
+                    <th>Proprietário</th>
+                    <th>Placa</th>
+                    <th>Chassi</th>
+                    <th>situação</th>
                 </tr>
-            <?php endforeach?>    
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($veiculos as $veiculo): ?>
+                    <tr>
+                        <td><?php echo $veiculo->proprietario; ?></td>
+                        <td><?php echo $veiculo->placa; ?></td>
+                        <td><?php echo $veiculo->chassi; ?></td>
+                        <td><?php echo $veiculo->situacao; ?></td>                    
+                    </tr>
+                <?php endforeach?>    
+            </tbody>
+        </table>
+    </div>
+    
 </body>
 </html>
